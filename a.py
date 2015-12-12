9febda32185537b5974f26b9ca15d181b839db36
null122 <714779883@qq.com>
2015-12-12 21:18:59 +0800

    BaseFunction

--- a.py
+++ a.py
@@ -3,19 +3,62 @@
 from urllib2 import urlopen
 import urllib2
 import re
-u='http://mm.taobao.com/json/request_top_list.htm?page='
-k=0
-for temp in range(50):
-    tu=u+str(temp)
-    req=urllib2.Request(tu)
-    url=urlopen(req)
-    RE=re.compile('<img src="//(.*?)"',re.S)
-    page=url.read().decode('gbk')
-    item=re.findall(RE,page)
-    for i in item:
-        h='http://'+i
-        img=urlopen(h)
-        data=img.read()
-        with open(str(k)+'.jpg','wb') as f:
-            f.write(data)
-        k=k+1
+import os
+from taobao import Taobao
+class Spider(object):
+    def __init__(self):
+        self.u='http://mm.taobao.com/json/request_top_list.htm?page='
+        self.rule_1='<div class="list-item">.*?<div class="pic-word">.*?<a href="//(.*?)".*?<img src="//(.*?)".*?<a class="lady-name" .*?>(.*?)</a>'
+        self.rule_2='<img.*?src="//(.*?)"'
+        self.rule_3='<div class="mm-aixiu-content".*?">(.*?)<!--'
+    def baseurl(self,i):
+        return self.u+str(i)
+    def read(self,url):
+        req=urllib2.Request(url)
+        url=urlopen(req)
+        page=url.read().decode('gbk')
+        #page=page.encode('utf-8')
+        return page
+    def compile(self,rule):
+        RE=re.compile(rule,re.S)
+        return RE
+    def mkdir(self,path):
+        path=path.strip()
+        if os.path.exists(path):
+            return False
+        else:
+            os.makedirs(path)
+            return True
+    def savePicutre(self,temp,path):
+        path=path.strip()
+        k=0
+        for i in temp:
+            print i
+            url=urlopen('http://'+i)
+            with open(path+'\\'+str(k)+'.jpg','wb') as f:
+                f.write(url.read())
+            k=k+1
+    def load(self,start,end):
+        if isinstance(start,int) and isinstance(end,int):
+            for i in range(start,end):
+                 item=re.findall(self.compile(self.rule_1),self.read(self.baseurl(i)))
+                 for temp in item:
+                     if self.mkdir(temp[2]):
+                         tao=Taobao('http://'+temp[0])
+                         p=tao.main()
+                         p=p.decode('gbk')
+                         p=p.encode('gbk')
+                         with open('a.html','w') as f:
+                             f.write(p)
+                         picpart=re.search(self.compile(self.rule_3),p)
+                         print picpart.group(1)
+                         pic=re.findall(self.compile(self.rule_2),picpart.group(1))
+                         print pic
+                         self.savePicutre(pic,temp[2])
+                     else:
+                         raise(Exception)
+        else:
+            raise(Exception)
+
+t=Spider()
+t.load(1,10)
